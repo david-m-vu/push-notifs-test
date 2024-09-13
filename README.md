97f6074c10e79a4ee10819e93396980589e31290
David Vu <david.vu@berkeley.edu>
2024-09-13 10:32:10 -0700

    Added Mckinley's changes and attempted to attach audio

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Testing push notifications with create react app and firebase cloud messaging
+
+# Showing Notifications Even When the Tab Is Closed
+
+This branch provides a proof of concept to show that it is possible to send notifications even when the website is closed. `client/public/firebase-messaging-sw.js` runs a loop that sends a notification every 5 seconds, and this loop continues running even after the user closes the tab.
+
+To run:
+```
+$ npm run build && serve -s build
+```
+Then open `http://localhost:3000`. If the notification loop doesn't automatically start, you might have to open devtools, go to Application > Service Workers, click "Unregister" on each service worker, and then reload the page. If you close the tab, the notifications will continue appearing. To stop the infinite loop, click "Unregister" on the service worker.
+
+If the user quits their browser, the notifications will stop. I don't think there's any way for us to deliver notifications when the browser is not running.
--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -9,13 +9,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // https://firebase.google.com/docs/web/setup#config-object
 // REPLACE WITH YOUR OWN CONFIG
 firebase.initializeApp({
-    apiKey: "AIzaSyD82dVDOwu-Y6EXLHyFzx0LTKMMTnJD7Jk",
-    authDomain: "push-notifications-ff03e.firebaseapp.com",
-    projectId: "push-notifications-ff03e",
-    storageBucket: "push-notifications-ff03e.appspot.com",
-    messagingSenderId: "192226570518",
-    appId: "1:192226570518:web:fb32d20ae665de3bab6037",
-    measurementId: "G-N8C94QW4R6"
+  apiKey: "AIzaSyD82dVDOwu-Y6EXLHyFzx0LTKMMTnJD7Jk",
+  authDomain: "push-notifications-ff03e.firebaseapp.com",
+  projectId: "push-notifications-ff03e",
+  storageBucket: "push-notifications-ff03e.appspot.com",
+  messagingSenderId: "192226570518",
+  appId: "1:192226570518:web:fb32d20ae665de3bab6037",
+  measurementId: "G-N8C94QW4R6"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -24,17 +24,51 @@ const messaging = firebase.messaging();
 
 // handles background messages (when app is minimized)
 messaging.onBackgroundMessage((payload) => {
-    console.log(
-      '[firebase-messaging-sw.js] Received background message ',
-      payload
-    );
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-      body: 'Background Message body.',
-      icon: '/logo512.png'
-    };
-  
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
-  
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  // Customize notification here
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/logo512.png',
+    sound: '/audio/alert_sound.mp3',
+    vibrate: [300, 100, 400],
+    requireInteraction: true,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+function showReminder() {
+  const notificationTitle = 'Reminder';
+  const notificationOptions = {
+    body: 'Here is your 5-second reminder',
+    icon: '/logo512.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+}
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close(); // Close the notification when clicked
+  const audio = new Audio('/audio/alert_sound.mp3'); // Path to your sound file
+  audio.play(); // Play the audio
+});
+
+
+// self.addEventListener('activate', function (event) {
+//   console.log('Service worker activated');
+
+//   showReminder();
+
+//   function loop() {
+//     setTimeout(() => {
+//       showReminder();
+//       loop();
+//     }, 5000); // 5 seconds
+//   }
+
+//   loop();
+// });
